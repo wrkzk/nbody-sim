c3bdfa4d9afaa0b10c42a5f9d072c16b8e11d833
Warren <wrkzk@protonmail.com>
2026-03-24 16:53:11 -0500

    Add simple softening factor of 1.0

--- src/engine.cpp
+++ src/engine.cpp
@@ -17,6 +17,7 @@ struct Body {
 // Time step and gravitational constants
 const float dt = 0.00005;
 const float G = 5000000.0f;
+const float softening = 1.0f;
 
 // Window width and height
 const int width = 1500;
@@ -27,19 +28,20 @@ int main() {
     InitWindow(width, height, "N-Body Physics Simulator");
 
     // Three bodies with interesting initial conditions respresenting the 3-body problem
-    Body b0 = { glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f), 30.0f, 4.0f };
-    Body b1 = { glm::vec3(10.0f, 0.0f, 0.0f), glm::vec3(0.0f, -2000.0f, 0.0f), 10.0f, 0.8f };
-    Body b2 = { glm::vec3(-10.0f, 0.0f, 0.0f), glm::vec3(0.0f, 2000.0f, 0.0f), 10.0f, 0.8f };
-    Body b3 = { glm::vec3(0.0f, 10.0f, 0.0f), glm::vec3(2000.0f, 0.0f, 0.0f), 10.0f, 0.8f };
-    Body b4 = { glm::vec3(0.0f, -10.0f, 0.0f), glm::vec3(-2000.0f, 0.0f, 0.0f), 10.0f, 0.8f };
+    Body b0 = { glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f), 20.0f, 2.0f };
+    Body b1 = { glm::vec3(10.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, -3000.0f), 10.0f, 0.8f };
+    Body b2 = { glm::vec3(-10.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 3000.0f), 10.0f, 0.8f };
+    Body b3 = { glm::vec3(0.0f, 0.0f, 10.0f), glm::vec3(3000.0f, 0.0f, 0.0f), 10.0f, 0.8f };
+    Body b4 = { glm::vec3(0.0f, 0.0f, -10.0f), glm::vec3(-3000.0f, 0.0f, 0.0f), 10.0f, 0.8f };
+    Body b5 = { glm::vec3(0.0f, 10.0f, 0.0f), glm::vec3(3000.0f, 0.0f, 0.0f), 10.0f, 0.8f };
 
 
     // All the bodies are contained in the world vector
-    std::vector<Body> world = {b1, b2, b3, b4};
+    std::vector<Body> world = {b0, b1, b2, b3, b4};
 
     // Set up raylib camera
     Camera3D camera = { 0 };
-    camera.position = (Vector3){ 0.0f, 10.0f, 10.0f };
+    camera.position = (Vector3){ 0.0f, 20.0f, 0.0f };
     camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
     camera.fovy = 45.0f;
@@ -48,7 +50,7 @@ int main() {
 
     float rotAngle = 45;
     float tiltAngle = 45;
-    float camDist = 30;
+    float camDist = 70;
 
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
@@ -65,11 +67,11 @@ int main() {
         // Panning logic
         Vector3 camPos = {0, 0, camDist};
         Vector2 curMousePos = GetMousePosition();
-        Vector2 mouseDelta = prevMousePos - curMousePos;
+        Vector2 mouseDelta = curMousePos - prevMousePos;
         prevMousePos = curMousePos;
 
         
-        rotAngle += mouseDelta.x * 0.25f;
+        rotAngle -= mouseDelta.x * 0.25f;
         tiltAngle += mouseDelta.y * 0.25f;
 
         if (tiltAngle > 89)
@@ -78,16 +80,11 @@ int main() {
         if (tiltAngle < 1)
             tiltAngle = 1;
 
-        Matrix tiltMatrix = MatrixRotateX(tiltAngle * GetFrameTime());
-        Matrix rotMatrix = MatrixRotateY(rotAngle * GetFrameTime());
+        Matrix tiltMatrix = MatrixRotateX(-tiltAngle * DEG2RAD);
+        Matrix rotMatrix = MatrixRotateY(rotAngle * DEG2RAD);
         Matrix rot = MatrixMultiply(tiltMatrix, rotMatrix);
 
         camera.position = Vector3Transform(camPos, rot);
-
-        //if (IsMouseButtonDown(0)) {
-        //    camera.target = GetScreenToWorld2D(Vector2Add(camera.offset, mouseDelta), camera);
-        //}
-        
 
 
         // ----------------------------------------------
@@ -106,7 +103,8 @@ int main() {
 
                     // Newton's Law of Universal Gravitation
                 float distance = glm::distance(world[i].position, world[j].position);
-                float force = G * ((world[i].mass * world[j].mass) / (distance * distance));
+                float force = G * ((world[i].mass * world[j].mass) /
+                        (distance * distance) + (softening * softening));
                     
                     // Find directional unit vector between centers of masses and add total force
                 glm::vec3 dir = glm::normalize(world[j].position - world[i].position);
@@ -132,7 +130,7 @@ int main() {
         ClearBackground(BLACK);
         BeginMode3D(camera);
 
-        DrawSphere( (Vector3){0, 0, 0}, 0.5f, BLUE);
+        DrawGrid(50, 1);
 
         // Draw each of the bodies at the correct position with a specified radius
         for (int i = 0; i < world.size(); i++) {
